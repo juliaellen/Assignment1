1f088fd113d26b1d4c75f830ba9833e9f7558a84
juliaellen <43542680+juliaellen@users.noreply.github.com>
2018-09-28 12:43:06 +0100

    Add files via upload

---
+++ LCA.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Node { 
+    int data; 
+    Node left, right; 
+  
+    Node(int value) { 
+        data = value; 
+        left = right = null; 
+    } 
+} 
+
