67cb2a58e9d5a8e1d0fe1eaa7a8ebe54d03c5d10
juliaellen <43542680+juliaellen@users.noreply.github.com>
2018-10-01 12:16:10 +0100

    Main LCA Class

--- LCA.java
+++ LCA.java
@@ -1,50 +1,25 @@
 //Main class
-public class LCA<Key extends Comparable<Key>>{
-
-	public class Node{
-		Node left, right;
-		Key key;		
-
-		public Node(Key key)
-		{
-			this.key = key;
-		}
-	}
-
-	public Node root;
-
-
-	public void put(Key key)
+public class LCA{
+ 	public Node search(Node root, int v1, int v2)
 	{
-		root = put(root, key);
+	 if(root.val == v1 || root.val == v2)
+	 {
+		 return root;
+	 }
+	 
+	 Node leftBranch = search(root.left, v1,v2);
+	 Node rightBranch = search(root.right,v1,v2);
+	 //If they both have an answer this is the LCA 
+	 if(leftBranch != null && rightBranch != null)
+	 {
+		 return root;
+	 }
+	 if(leftBranch != null) // find and return the node with the value returns null if neither have the value
+	 {
+		 return leftBranch;
+		
+	 }
+	 else 
+		 return rightBranch;
 	}
-
-	private Node put(Node x, Key key)
-	{
-		if(x==null){
-			return new Node(key);
-		}
-		int cmp = key.compareTo(x.key);
-		if(cmp<0){
-			x.left = put(x.left, key);
-		}else if(cmp>0){
-			x.right = put(x.right, key);
-		}
-		return x;
-	}
-
-	public Node get(Key key)
-	{
-		return get(root, key);
-		}
-
-	private Node get(Node x, Key key) {
-		if (x == null) return null;
-		int cmp = key.compareTo(x.key);
-		if      (cmp < 0) return get(x.left, key);
-		else if (cmp > 0) return get(x.right, key);
-		else              return x;
-	}
-
-
-}
+ }
