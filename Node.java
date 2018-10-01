3e4fc98862194b827d4640599d778bd156eb2e98
juliaellen <43542680+juliaellen@users.noreply.github.com>
2018-10-01 12:22:33 +0100

    Carry out testing with asset null now resolve

--- Node.java
+++ Node.java
@@ -1,8 +1,68 @@
-
-public class Node {
-
-	public int val;
-	public Node left;
-	public Node right;
-
-}
+public class Node{
+		Node left, right;
+		Key key;		
+		
+ 		public Node(Key key)
+		{
+			this.key = key;
+		}
+	}
+ 	public Node root;
+	
+	
+ 	public void put(Key key)
+	{
+		root = put(root, key);
+// -34,32 +34,37 @@ private Node put(Node x, Key key)
+		}
+		return x;
+	}
+	
+ 	
+	
+	
+ 	public Node search(Node root, Key v1, Key v2)
+	{
+ 		if(root.key.compareTo(v1) == 0 || root.key.compareTo(v2) == 0)
+ 		 {
+ 			 return root;
+ 		 }
+ 		 
+ 		 Node leftBranch = search(root.left, v1,v2);
+ 		 Node rightBranch = search(root.right,v1,v2);
+ 		 //If they both have an answer this is the LCA 
+ 		 if(leftBranch != null && rightBranch != null)
+ 		 {
+ 			 return root;
+ 		 }
+ 		 if(leftBranch != null) // find and return the node with the value returns null if neither have the value
+ 		 {
+ 			 return leftBranch;
+ 			
+ 		 }
+ 		 else 
+ 			 return rightBranch;
+ 		}
+ 			if(root != null)
+ 			{
+ 	 			if(root.key.compareTo(v1) == 0 || root.key.compareTo(v2) == 0)
+ 				{
+ 					return root;
+ 				}
+ 	 			Node leftBranch = search(root.left, v1,v2);
+ 				Node rightBranch = search(root.right,v1,v2);
+ 				//If they both have an answer this is the LCA 
+ 				if(leftBranch != null && rightBranch != null)
+ 				{
+ 					return root;
+ 				}
+ 				if(leftBranch != null) // find and return the node with the value returns null if neither have the value
+ 				{
+ 					return leftBranch;
+ 	 			}
+ 				else 
+ 					return rightBranch;
+ 			}
+ 			return null;
+ 		}
+ 	}
