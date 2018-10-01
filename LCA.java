a4552de0666c931f6651fcb8ab3efa86c376ef27
juliaellen <43542680+juliaellen@users.noreply.github.com>
2018-10-01 12:18:13 +0100

    LCA&BCA classes consolidated

--- LCA.java
+++ LCA.java
@@ -1,25 +1,44 @@
 //Main class
-public class LCA{
+//public class LCA{
+public class LCA<Key extends Comparable<Key>>{
+ 	public class Node{
+		Node left, right;
+		Key key;		
+		
+		public Node(Key key)
+		{
+			this.key = key;
+		}
+	}
+ 	public Node root;
+	
+	
+	public void put(Key key)
+	{
+		root = put(root, key);
+	}
+ 	private Node put(Node x, Key key)
+	{
+		if(x==null){
+			return new Node(key);
+		}
+		int cmp = key.compareTo(x.key);
+		if(cmp<0){
+			x.left = put(x.left, key);
+		}else if(cmp>0){
+			x.right = put(x.right, key);
+		}
+		return x;
+	}
+	
  	public Node search(Node root, int v1, int v2)
+	
+	
+	
+	public Node search(Node root, Key v1, Key v2)
 	{
 	 if(root.val == v1 || root.val == v2)
+	 if(root.key.compareTo(v1) == 0 || root.key.compareTo(v2) == 0)
 	 {
 		 return root;
 	 }
-	 
-	 Node leftBranch = search(root.left, v1,v2);
-	 Node rightBranch = search(root.right,v1,v2);
-	 //If they both have an answer this is the LCA 
-	 if(leftBranch != null && rightBranch != null)
-	 {
-		 return root;
-	 }
-	 if(leftBranch != null) // find and return the node with the value returns null if neither have the value
-	 {
-		 return leftBranch;
-		
-	 }
-	 else 
-		 return rightBranch;
-	}
- }
