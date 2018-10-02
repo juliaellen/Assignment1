0e396ca4946cce6d41ecf95d78d9f14b726a0e82
juliaellen <43542680+juliaellen@users.noreply.github.com>
2018-10-02 12:03:26 +0100

    Outline of tests I am doing

--- LCATEST.java
+++ LCATEST.java
@@ -1,47 +1,29 @@
-import org.junit.Test;
- //Using JUNIT FrameWork
-public class LCATEST {
- 	
-	@Test // test with no nodes
+import static org.junit.Assert.*;
+ import org.junit.Test;
+ public class LCATEST {
+ 	@Test // test with no nodes
 	public void testEmpty() {
-		BST<Integer> testBST = new BST<Integer>();
 		
-		assertNull( LCA(testBST.root, 7, 1));
 	}
  	@Test // test with only one node in
 	public void testSingle()
 	{
-		BST<Integer> testBST = new BST<Integer>();
-		testBST.put(7);
-		assertNull(LCA(testBST.root,1,3));
 		
 	}
 	
 	@Test // test with multiple nodes
 	public void testPopulated()
 	{
-		BST<Integer> test = new BST<Integer>();
-		test.put(9);
-		test.put(1);
-		test.put(4);
-		test.put(19);
-		test.put(9);
-		test.put(13);
 		
 	}
 	@Test // test when left or right are not in the tree
 	public void testNotInTree()
 	{
 		
- 		BST<Integer> test = new BST<Integer>();
-		test.put(9);
-		test.put(1);
-		test.put(4);
-		test.put(19);
-		test.put(9);
-		test.put(13);
-		assertNull(LeastCommonAncestor(test.root,19,3));
 	}
 	@Test // test when v1 and v2 are the same value
 	public void testSameValue()
-
+	{
+		
+	}
+}
