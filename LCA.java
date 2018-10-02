d865afe3d81036c4fc42508d0b7646e80a4b0460
juliaellen <43542680+juliaellen@users.noreply.github.com>
2018-10-02 11:59:45 +0100

    Final LCA Class

--- LCA.java
+++ LCA.java
@@ -1,7 +1,52 @@
-
-
 //Main class
 public class LCA<Key extends Comparable<Key>>{
+
+	public class Node{
+		Node left, right;
+		Key key;		
+
+		public Node(Key key)
+		{
+			this.key = key;
+		}
+	}
+
+	public Node root;
+
+
+	public void put(Key key)
+	{
+		root = put(root, key);
+	}
+
+	private Node put(Node x, Key key)
+	{
+		if(x==null){
+			return new Node(key);
+		}
+		int cmp = key.compareTo(x.key);
+		if(cmp<0){
+			x.left = put(x.left, key);
+		}else if(cmp>0){
+			x.right = put(x.right, key);
+		}
+		return x;
+	}
+
+	public Node get(Key key)
+	{
+		return get(root, key);
+		}
+
+	private Node get(Node x, Key key) {
+		if (x == null) return null;
+		int cmp = key.compareTo(x.key);
+		if      (cmp < 0) return get(x.left, key);
+		else if (cmp > 0) return get(x.right, key);
+		else              return x;
+	}
+
+
 
 
 	public Node search(Node root, Key v1, Key v2)
