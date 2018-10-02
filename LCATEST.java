8c4cb8f1e083dd7a73e266e21064ae4afd034d12
juliaellen <43542680+juliaellen@users.noreply.github.com>
2018-10-02 12:20:05 +0100

    Added Not In Tree

--- LCATEST.java
+++ LCATEST.java
@@ -19,11 +19,25 @@ import static org.junit.Assert.*;
 	@Test // test when left or right are not in the tree
 	public void testNotInTree()
 	{
+			
+	 		BST<Integer> test = new BST<Integer>();
+			test.put(9);
+			test.put(1);
+			test.put(4);
+			test.put(19);
+			test.put(9);
+			test.put(13);
+			assertNull(LeastCommonAncestor(test.root,19,3));
+	
 		
 	}
 	@Test // test when v1 and v2 are the same value
+	
 	public void testSameValue()
-	{
-		
+
+		test.put(9);
+		test.put(13);
+		assertSame(test.search(test.root,9,9).key,9);
+		assertSame(test.search(test.root,19,19).key,19);
 	}
 }
