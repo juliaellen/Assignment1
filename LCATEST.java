5306420e723fd898ff357292404a905679d76e5d
juliaellen <43542680+juliaellen@users.noreply.github.com>
2018-10-12 14:16:56 +0100

    Completed LCATEST class- all changes included

--- LCATEST.java
+++ LCATEST.java
@@ -1,82 +1,90 @@
 import static org.junit.Assert.*;
+
 import org.junit.Test;
+
+//Using JUNIT FrameWork
 public class LCATEST {
-
-
+	
 	@Test // test with no nodes
 	public void testEmpty() {
 		LCA<Integer> testBST = new LCA<Integer>();
-		LCA testBST = new LCA();
-
+		
 		assertNull( testBST.search(testBST.root, 7, 1));
 	}
 
+	@Test // test with only one node in
 	public void testSingle()
 	{
-
+		LCA<Integer> testBST = new LCA<Integer>();
+		testBST.put(7);
+		assertNull(testBST.search(testBST.root,1,3));
+		
 	}
-
-	public void testGet()
-	{
-		LCA<Integer> bst = new LCA<Integer>(); 	
-		assertSame("test get empty tree", null, bst.get(7));
-		assertNull("test get empty tree", bst.get(7));
-		bst.put(1);
-		assertSame("test single tree", 1, bst.get(1));
-		assertSame("test single tree", 1, bst.get(1).key);
-		bst.put(2);
-		bst.put(4);
-		bst.put(5);
-		assertSame("test value larger than root",4, bst.get(4));
-		assertSame("test value smaller than root", 2, bst.get(2));
-		assertSame("test value larger than root",4, bst.get(4).key);
-		assertSame("test value smaller than root", 2, bst.get(2).key);
-		assertSame("test value larger but not in tree", null, bst.get(11));
-	}
-
+	
 	@Test // test with multiple nodes
 	public void testPopulated()
 	{
-
-	}
-	@Test // test when left or right are not in the tree
-	public void testNotInTree()
-	{
-
-		BST<Integer> test = new BST<Integer>();
+		LCA<Integer> test = new LCA<Integer>();
 		test.put(9);
 		test.put(1);
 		test.put(4);
 		test.put(19);
 		test.put(9);
 		test.put(13);
-		assertNull(LeastCommonAncestor(test.root,19,3));
+		test.put(3);
+		test.put(2);
+		test.put(15);
+		test.put(14);
+		test.put(16);
+		
+		assertSame(test.search(test.root,19,1).key,9);
+		assertSame(test.search(test.root,19,9).key,9);
+		assertSame(test.search(test.root,14,16).key,15);
+	}
+	@Test // test when left or right are not in the tree
+	public void testNotInTree()
+	{
 
-
+		LCA<Integer> test = new LCA<Integer>();
+		test.put(9);
+		test.put(1);
+		test.put(4);
+		test.put(19);
+		test.put(9);
+		test.put(13);
+		assertNull(test.search(test.root,19,3)); // returns Null so cannot compare key or triggers nullpointer
+		assertNull(test.search(test.root,1,20));
+		assertNull(test.search(test.root,5,3));
+		assertNull(test.search(test.root,5,5));
+		assertNull(test.search(test.root,20,1));
 	}
 	@Test // test when v1 and v2 are the same value
-
 	public void testSameValue()
+	{
+		LCA<Integer> test = new LCA<Integer>();
+		test.put(9);
+		test.put(1);
+		test.put(4);
+		test.put(19);
+		test.put(9);
+		test.put(13);
+		assertSame(test.search(test.root,9,9).key,9);
+		assertSame(test.search(test.root,19,19).key,19);
+	}
+	
+	@Test
+	public void testGet()
+	{
+		LCA<Integer> bst = new LCA<Integer>(); 	
+		assertNull("test get empty tree", bst.get(7));
+		bst.put(1);
+		assertSame("test single tree", 1, bst.get(1).key);
+		bst.put(2);
+		bst.put(4);
+		bst.put(5);
+		assertSame("test value larger than root",4, bst.get(4).key);
+		assertSame("test value smaller than root", 2, bst.get(2).key);
+		assertSame("test value larger but not in tree", null, bst.get(11));
+	}
 
-	test.put(9);
-	test.put(13);
-	assertSame(test.search(test.root,9,9).key,9);
-	assertSame(test.search(test.root,19,19).key,19);
-}
-@Test 
-public void testIsAcyclic()
-{
-	LCA test = new LCA();
-	Node b = new Node(1,"B");
-	test.put(b);
-	assertTrue(test.isAcyclic());// test single node
-	Node a = new Node(1,"A");
-	test.put(a);
-	b.addEdgeTo(a);
-	assertTrue(test.isAcyclic); // test two nodes acyclic
-	a.addEdgeTo(b);
-	assertFalse(test.isAcyclic); // test two nodes cyclic
-
-
-}
 }
