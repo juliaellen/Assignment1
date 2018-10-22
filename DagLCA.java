a302a33b55681233d80df3fb9473c568825e7ae8
juliaellen <43542680+juliaellen@users.noreply.github.com>
2018-10-22 12:34:25 +0100

    more code for dag lca

--- DagLCA.java
+++ DagLCA.java
@@ -31,5 +31,49 @@ public class DagLCA {
 		boolean[] marked = new boolean[V];
 		boolean[] stack = new boolean[V];
 	}
+	
+	public Node root;
+ 	public void put(Key key)
+	{
+		root = put(root, key);
+	}
+ 	private Node put(Node x, Key key)
+	{
+		if(x==null){
+			return new Node(key);
+		}
+		int cmp = key.compareTo(x.key);
+		if(cmp<0){
+			x.left = put(x.left, key);
+		}else if(cmp>0){
+			x.right = put(x.right, key);
+		}
+		return x;
+	/**
+	 * Returns the number of vertices in this digraph.
+	 *
+	 * @return the number of vertices in this digraph
+	 */
+	public int V() {
+		return V;
+	}
+ 	public Node get(Key key)
+	{
+		return get(root, key);
+		}
+ 	private Node get(Node x, Key key) {
+		if (x == null) return null;
+		int cmp = key.compareTo(x.key);
+		if      (cmp < 0) return get(x.left, key);
+		else if (cmp > 0) return get(x.right, key);
+		else              return x;
+	/**
+	 * Returns the number of edges in this digraph.
+	 *
+	 * @return the number of edges in this digraph
+	 */
+	public int E() {
+		return E;
+	}
 
 }
