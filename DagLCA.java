969fb59da57333b5da27a31d7ff47cbcb64a702f
juliaellen <43542680+juliaellen@users.noreply.github.com>
2018-10-26 15:32:40 +0100

    Testing LCA using DFS

--- DagLCA.java
+++ DagLCA.java
@@ -1,192 +1,121 @@
+
 import java.util.ArrayList;
-public class DagLCA {
-
-	//I am using a graph implementation similar to 
-	//https://algs4.cs.princeton.edu/42digraph/Digraph.java.html
-	//I am not trying to reinvent the wheel as was suggested to us
-
-	private final int V;           // number of vertices in this digraph
-	private int E;                 // number of edges in this digraph
-	private ArrayList<Integer>[] adj;    // adj[v] = adjacency list for vertex v
-	private int[] indegree;        // indegree[v] = indegree of vertex v
+import java.util.Iterator;
+import java.util.LinkedList;
+ //mainclass
+public class DagLCA{
+@@ -12,6 +14,14 @@
 	public boolean isDAG;
 	public boolean[] marked; //boolean array for visited vertices
 	public boolean[] stack; 
+	
+	// For DFS 
+	public int[]pre;   
+    public int[]post  ;
+    public ArrayList<Integer> postorder;
+    public ArrayList<Integer> preorder ;
+    public int preCounter;
+    public int postCounter;
 	/**
 	 * Initializes an empty digraph with <em>V</em> vertices.
 	 *
-	 * @param  V the number of vertices
-	 * @throws IllegalArgumentException if {@code V < 0}
-	 */
-	public DagLCA(int V) 
-	{
-		if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
-		this.V = V;
-		this.E = 0;
-		indegree = new int[V];
+@@ -26,11 +36,19 @@ public DagLCA(int V) {
 		adj = (ArrayList<Integer>[]) new ArrayList[V];
-		for (int v = 0; v < V; v++) 
-		{
+		for (int v = 0; v < V; v++) {
 			adj[v] = new ArrayList<Integer>();
+			
+		
 		}
 		isDAG = true;
-		boolean[] marked = new boolean[V];
-		boolean[] stack = new boolean[V];
+		marked = new boolean[V];
+		stack = new boolean[V];
+	
+		//for DFS
+		pre    = new int[V()];
+	    post   = new int[V()];
+	    postorder = new ArrayList<Integer>();
+	    preorder  = new ArrayList<Integer>();
+	    preCounter=0;
+	    postCounter=0;
 	}
-
-	public Node root;
-	public void put(Key key)
-	{
-		root = put(root, key);
-	}
-	private Node put(Node x, Key key)
-	{
-		if(x==null){
-			return new Node(key);
-		}
-		int cmp = key.compareTo(x.key);
-		if(cmp<0){
-			x.left = put(x.left, key);
-		}else if(cmp>0){
-			x.right = put(x.right, key);
-		}
-		return x;
-		/**
-		 * Returns the number of vertices in this digraph.
-		 *
-		 * @return the number of vertices in this digraph
-		 */
-		public int V() 
-		{
-			return V;
-		}
-		public Node get(Key key)
-		{
-			return get(root, key);
-		}
-		private Node get(Node x, Key key) 
-		{
-			if (x == null) return null;
-			int cmp = key.compareTo(x.key);
-			if      (cmp < 0) return get(x.left, key);
-			else if (cmp > 0) return get(x.right, key);
-			else              return x;
-			/**
-			 * Returns the number of edges in this digraph.
-			 *
-			 * @return the number of edges in this digraph
-			 */
-			public int E()
-			{
-				return E;
-			}
-
-			// throw an IllegalArgumentException unless {@code 0 <= v < V}
-			private void validateVertex(int v) 
-			{
-				if (v < 0 || v >= V)
-					throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
-			}
-		}
-		/**
-		 * Adds the directed edge v-w to this digraph.
-		 *
-		 * @param  v the tail vertex
-		 * @param  w the head vertex
-		 * @throws IllegalArgumentException unless both {@code 0 <= v < V} and {@code 0 <= w < V}
-		 */
-		public void addEdge(int v, int w) 
-		{
-			validateVertex(v);
-			validateVertex(w);
-			adj[v].add(w);
-			indegree[w]++;
-			E++;
-		}
-		public Node search(Node root, Key v1, Key v2)
-		{
-			if(root != null && (get(v1)!=null && get(v2)!=null))
-			{
-				/**
-				 * Returns the vertices adjacent from vertex {@code v} in this digraph.
-				 *
-				 * @param  v the vertex
-				 * @return the vertices adjacent from vertex {@code v} in this digraph, as an iterable
-				 * @throws IllegalArgumentException unless {@code 0 <= v < V}
-				 */
-				public Iterable<Integer> adj(int v) {
-					validateVertex(v);
-					return adj[v];
-				}
-				if(root.key.compareTo(v1) == 0 || root.key.compareTo(v2) == 0)
-				{
-					return root;
-				}
-				Node leftBranch = search(root.left, v1,v2);
-				Node rightBranch = search(root.right,v1,v2);
-				//If they both have an answer this is the LCA 
-				if(leftBranch != null && rightBranch != null)
-				{
-					return root;
-				}
-				if(leftBranch != null) // find and return the node with the value returns null if neither have the value
-				{
-					return leftBranch;
-					public boolean acyclic()
-					{
-						return isDAG;	
-					}
-					//Sedgewick
-					public void acyclic(int v)
-					{
-						stack[v] =true; 
-						marked[v] = true;
-						for (int w : adj(v)) {
-							if(!marked[w]) {
-								acyclic(w);
-							} else if (stack[w]) {
-								isDAG = false;
-								return;
-							}
-							else if(rightBranch != null)
-							{
-								return rightBranch;
-							}
-							else 
-								return null;
-						}
-						return null;
-						stack[v] = false;
-					}
-				}
-			}
-		}
-	}
-	private void validateVertex(int v) 
-	{
+ @@ -60,7 +78,7 @@ public int E() {
+	private void validateVertex(int v) {
 		if (v < 0 || v >= V)
 			throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
-
+		
+ 	}
+ 	/**
+@@ -121,36 +139,85 @@ private void acyclic(int v)
+		}
+		stack[v] = false;
 	}
-
-
-	public int LCA(int v, int w)
+	
+ 	public int LCA(int v, int w)
 	{
+		boolean haveLCA =false;
+		//Will be acyclic if reversed so no need to reverse yet
 		isAcyclic();
 		if(!isDAG){
 			return -1;
 		}
 		validateVertex(v);//exception thrown ifinvalid
 		validateVertex(w);
-
-
-
+		validateVertex(w);//check if they exist
 		if(E==0)// no edges
 		{
 			return -1;
 		}
+		//is valid to search now reverse
+ 		DagLCA  reversed = this.reverse();
+		ArrayList<Integer> commonAncestors = new ArrayList<Integer>();
+		
+		reversed.dfs(v);        //Testing DFS first to see if it works       
+		ArrayList<Integer> childVSearch =postorder;
+		marked= new boolean[V()];
+		
+		pre    = new int[V()];
+	    post   = new int[V()];
+	    postorder = new ArrayList<Integer>();
+	    preorder  = new ArrayList<Integer>();
+	    preCounter=0;
+	    postCounter=0;
+	    reversed.dfs(w);
+		ArrayList<Integer> childWSearch= postorder;
+		for(int i = 0; i<childVSearch.size(); i++)
+		{
+			for(int t = 0; t<childWSearch.size(); t++)
+			{		
+				if(childVSearch.get(i)==childWSearch.get(t))
+				{ // first occurence of a match is where the lowest common 
+					commonAncestors.add(childVSearch.get(i));	
+					haveLCA = true;
+				}
+			}
+			
+			
+		}
+		if(haveLCA)
+			return commonAncestors.get(0);
+		else
+			
+			return -1;
 	}
-	public DagLCA reverse() {
+	//reverse the DAG so we can navigate to parent nodes. cant be bothered incorporating parents with the add edge if thats even allowed
+ 	
+	   private void dfs(int v) {
+	        marked[v] = true;
+	        pre[v] = preCounter++;
+	        preorder.add(v);
+	        for (int w : adj(v)) {
+	            if (!marked[w]) {
+	                dfs(w);
+	            }
+	        }
+	        postorder.add(v);
+	        post[v] = postCounter++;
+	    }
+	//reverse the DAG so we can navigate to parent nodes. cant be bothered incorporating parents with the add edge if thats even allowed
+ 	public DagLCA reverse() {
         DagLCA childToParent = new DagLCA(V);
         for (int v = 0; v < V; v++) 
         {
@@ -198,6 +127,14 @@ public class DagLCA {
         }
         return childToParent;
     }
+		DagLCA childToParent = new DagLCA(V);
+		for (int v = 0; v < V; v++) 
+		{
+ 			for (int w : adj(v)) 
+			{
+				childToParent.addEdge(w, v);
+			}
+		}
+		return childToParent;
+	}
 }
-
-
