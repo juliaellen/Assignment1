390d2d03914834986375d0f5872d0a04f49c8f08
juliaellen <43542680+juliaellen@users.noreply.github.com>
2018-10-26 15:38:38 +0100

    Finished updating tests

--- DagLCATEST.java
+++ DagLCATEST.java
@@ -1,15 +1,19 @@
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
+
+import org.junit.Test;
+
 //Using JUNIT FrameWork
-public class DagLCATEST 
-{
+public class DagLCATest {
 
 	// testing functions based on DAG similar to SEdgwick AdjMatrix implementation
 	@Test
-	public test
 	public void testV()
 	{
 		DagLCA test1 = new DagLCA(20);
 		assertEquals("testing V() function", 20, test1.V());
+
 	}
 	@Test
 	public void testE()
@@ -19,6 +23,7 @@ public class DagLCATEST
 		test1.addEdge(1, 2);
 		assertEquals("testing with no edges",1,test1.E());
 	}
+
 	@Test
 	public void testAcyclic()
 	{
@@ -26,13 +31,13 @@ public class DagLCATEST
 		test1.addEdge(1, 2);
 		test1.addEdge(2, 3);
 		test1.addEdge(3, 4);
-		assertTrue(test1.acyclic())
+		test1.isAcyclic();
 		assertTrue(test1.acyclic());
 		test1.addEdge(4,1);
+		test1.isAcyclic();
 		assertFalse(test1.acyclic());
-
 	}
-
+	
 	@Test
 	public void testLCA()
 	{
@@ -41,7 +46,16 @@ public class DagLCATEST
 		test1.addEdge(1, 2);
 		test1.addEdge(2, 3);
 		test1.addEdge(3, 4);
-@@ -58,6 +59,9 @@ public void testLCA()
+		test1.addEdge(3, 5);
+		assertEquals("We know it it 3",3,test1.LCA(4,5));
+		assertEquals("We know it it 3",3,test1.LCA(5,4));
+		test1.addEdge(5, 6);
+		test1.addEdge(4, 7);
+		test1.addEdge(7,8);
+		test1.addEdge(6, 9);
+		test1.addEdge(9,10);//joins the branches
+		test1.addEdge(8, 10);
+		assertEquals("LCA should be at the branch poin 3",3, test1.LCA(9,7));
 		assertEquals("LCA of two of the same line", 7,test1.LCA(7,8));
 		assertEquals("LCA of two of the same line", 4,test1.LCA(7,4));
 		assertEquals("LCA of two of the same line", 4,test1.LCA(4,7));
@@ -53,13 +67,28 @@ public class DagLCATEST
 	@Test(expected = IllegalArgumentException.class) 
 	public void testExceptionThrowers() // tested through the adj function giving errors
 	{
-
 		DagLCA test = new DagLCA(6);
 		test.adj(-1);
 		test.adj(88);
-		test.LCA(1, -1);
-		test.LCA(1, 88);
+		test.LCA(1, -1);//not possible to have it 
+		test.LCA(1, 88);//and the vertex is out of bounds
 		DagLCA test2 = new DagLCA(-4); //shouldnt have negative vertices for our purposes
 	}
-
+	
+	@Test 
+	public void testReverse()
+	{
+		DagLCA test1 = new DagLCA(10);
+		test1.addEdge(1, 2);
+		test1.addEdge(0, 1);
+		DagLCA testReverse = test1.reverse();
+		assertSame("Edges should be the same", testReverse.E(),test1.E());
+		assertSame("shouldnt lose vertices", testReverse.V(),test1.V());
+		DagLCA test2 = new DagLCA(25);
+		DagLCA rev2 = test2.reverse();
+		assertSame("Edges should be the same", rev2.E(),test2.E());
+		assertSame("shouldnt lose vertices", rev2.V(),test2.V());
+	}
+	
+	
 }
