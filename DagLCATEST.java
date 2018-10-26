01b73fdf6611e0b7f3bd2fd6e5ebb174d1785d92
juliaellen <43542680+juliaellen@users.noreply.github.com>
2018-10-26 15:37:54 +0100

    Updated some tests

--- DagLCATEST.java
+++ DagLCATEST.java
@@ -36,22 +36,19 @@ public class DagLCATEST
 	@Test
 	public void testLCA()
 	{
-		DagLCA test1 = new DagLCA(10);
+		DagLCA test1 = new DagLCA(15);
+		assertEquals("both vertices exist but not connected", -1,test1.LCA(1, 2));
 		test1.addEdge(1, 2);
 		test1.addEdge(2, 3);
 		test1.addEdge(3, 4);
-		test1.addEdge(3, 5);
-		assertEquals("We know it it 3",3,test1.LCA(4,5));
-		assertEquals("We know it it 3",3,test1.LCA(5,4));
-		test1.addEdge(5, 6);
-		test1.addEdge(4, 7);
-		test1.addEdge(7,8);
-		test1.addEdge(6, 9);
-		test1.addEdge(9,10);//joins the branches
-		test1.addEdge(8, 10);
-		assertEquals("LCA should be at the branch poin 3",3, test1.LCA(9,7));
+@@ -58,6 +59,9 @@ public void testLCA()
 		assertEquals("LCA of two of the same line", 7,test1.LCA(7,8));
-
+		assertEquals("LCA of two of the same line", 4,test1.LCA(7,4));
+		assertEquals("LCA of two of the same line", 4,test1.LCA(4,7));
+		assertEquals("when one is in the DAG but not connected", -1,test1.LCA(13, 4));
+		test1.addEdge(9, 1);
+		assertEquals("LCA when it is cyclical",-1,test1.LCA(3, 1));
+		
 	}
 	@Test(expected = IllegalArgumentException.class) 
 	public void testExceptionThrowers() // tested through the adj function giving errors
